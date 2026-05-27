05b12a28fcb0efcab5bcf70984a3fcc239ee4f8b
Arungrandhi <arungrandhip@gmail.com>
2026-05-27 10:00:11 +0530

    Update CORS configuration to allow multiple origins

--- app.js
+++ app.js
@@ -16,9 +16,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://school-college-erp.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
